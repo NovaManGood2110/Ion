a1c9ab71cfbe91f699eb68d245b2725a280efed8
NovaManGood2110 <128265875+NovaManGood2110@users.noreply.github.com>
2023-09-11 18:31:52 -0500

    Update ChaingunProjectile.kt

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/ChaingunProjectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/ChaingunProjectile.kt
@@ -1,1 +1,28 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile
 
+import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.features.starship.controllers.Controller
+import net.horizonsend.ion.server.miscellaneous.utils.gayColors
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import org.bukkit.Color
+import org.bukkit.Location
+import org.bukkit.util.Vector
+
+// from left to right red - orange - yellow - green - blue - purple
+class ChaingunProjectile(
+	starship: ActiveStarship,
+	loc: Location,
+	dir: Vector,
+	shooter: Controller?
+) : LaserProjectile(starship, loc, dir, shooter) {
+	override val range: Double = IonServer.balancing.starshipWeapons.Chaingun.range
+	override val speed: Double = IonServer.balancing.starshipWeapons.Chaingun.speed
+	override val shieldDamageMultiplier: Int = IonServer.balancing.starshipWeapons.Chaingun.shieldDamageMultiplier
+	override val color: Color
+		get() = if (starship!!.rainbowToggle) gayColors.random() else starship.weaponColor
+	override val thickness: Double = IonServer.balancing.starshipWeapons.Chaingun.thickness
+	override val particleThickness: Double = IonServer.balancing.starshipWeapons.Chaingun.particleThickness
+	override val explosionPower: Float = IonServer.balancing.starshipWeapons.Chaingun.explosionPower
+	override val volume: Int = IonServer.balancing.starshipWeapons.Chaingun.volume
+	override val soundName: String = IonServer.balancing.starshipWeapons.Chaingun.soundName
+}
