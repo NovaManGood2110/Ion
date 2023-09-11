581edb83a4fc259fc636b979db4b62cc492ffe53
NovaManGood2110 <128265875+NovaManGood2110@users.noreply.github.com>
2023-09-11 17:57:27 -0500

    Update ChaingunWeaponSubsystem.kt

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/ChaingunWeaponSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/primary/ChaingunWeaponSubsystem.kt
@@ -1,1 +1,39 @@
+package net.horizonsend.ion.server.features.starship.subsystem.weapon.primary
 
+import net.horizonsend.ion.server.IonServer
+import net.horizonsend.ion.server.features.starship.controllers.Controller
+import net.horizonsend.ion.server.features.starship.active.ActiveStarship
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.CannonWeaponSubsystem
+import net.horizonsend.ion.server.features.starship.subsystem.weapon.projectile.PlasmaLaserProjectile
+import net.horizonsend.ion.server.miscellaneous.utils.Vec3i
+import org.bukkit.Location
+import org.bukkit.block.BlockFace
+import org.bukkit.util.Vector
+
+class ChaingunWeaponSubsystem(starship: ActiveStarship, pos: Vec3i, face: BlockFace) :
+	CannonWeaponSubsystem(starship, pos, face) {
+	override val powerUsage: Int = IonServer.balancing.starshipWeapons.Chaingun.powerUsage
+	override val length: Int = IonServer.balancing.starshipWeapons.Chaingun.length
+	override val angleRadians: Double = Math.toRadians(IonServer.balancing.starshipWeapons.Chaingun.angleRadians)
+	override val convergeDist: Double = IonServer.balancing.starshipWeapons.Chaingun.convergeDistance
+	override val extraDistance: Int = IonServer.balancing.starshipWeapons.Chaingun.extraDistance
+
+	override fun isAcceptableDirection(face: BlockFace): Boolean {
+		return true
+	}
+
+	override fun isForwardOnly(): Boolean = IonServer.balancing.starshipWeapons.Chaingun.fowardOnly
+
+	override fun getMaxPerShot(): Int {
+		return 1
+	}
+
+	override fun fire(
+        loc: Location,
+        dir: Vector,
+        shooter: Controller,
+        target: Vector?
+	) {
+		PlasmaLaserProjectile(starship, loc, dir, shooter).fire()
+	}
+}
