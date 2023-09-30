585f186bafa734af09d79f3ea5371fc95a0e02ae
NovaManGood2110 <128265875+NovaManGood2110@users.noreply.github.com>
2023-09-30 18:08:35 -0500

    Update ShieldSubsystem.kt

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/shield/ShieldSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/shield/ShieldSubsystem.kt
@@ -44,7 +44,7 @@ abstract class ShieldSubsystem(
 
     fun getPowerUsage(power: Double): Int {
         return if (starship.initialBlockCount < 1000)
-            (power * 7000.0).toInt()
+            (power * 10000.0).toInt()
         else (power * 3000.0).toInt()
     }
 
