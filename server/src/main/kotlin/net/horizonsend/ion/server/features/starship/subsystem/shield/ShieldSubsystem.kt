09695563098a9cb780a5e2cc5eb19913fb7c50e6
kwazedilla <kwazedilla@gmail.com>
2023-09-29 17:53:11 -0700

    formatting fixes

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/shield/ShieldSubsystem.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/shield/ShieldSubsystem.kt
@@ -11,42 +11,42 @@ import kotlin.math.pow
 import kotlin.math.roundToInt
 
 abstract class ShieldSubsystem(
-	starship: ActiveStarship,
-	sign: Sign,
-	multiblock: ShieldMultiblock
-	) : AbstractMultiblockSubsystem<ShieldMultiblock>(starship, sign, multiblock) {
-	    val name: String = sign.getLine(2).stripColor()
-	    val maxShields: Double = (0.00671215 * starship.initialBlockCount.toDouble().pow(0.836512) - 0.188437)
-	        get() = if (starship.initialBlockCount < 500) (1 - field + (1)) else field
-	    val maxPower: Int = (starship.initialBlockCount.coerceAtLeast(500).d().pow(3.0 / 5.0) * 10000.0).roundToInt()
-	        get() = if (starship.shields.size > maxShields) {
-	            ((maxShields / starship.shields.size) * field).toInt()
-	        } else if (starship.initialBlockCount <= 1000) {
-	             (field/2)
-	        } else if (starship.type.eventship) {
-	            (field * 2)
-	        } else {
-	            field
-	        }
+    starship: ActiveStarship,
+    sign: Sign,
+    multiblock: ShieldMultiblock
+) : AbstractMultiblockSubsystem<ShieldMultiblock>(starship, sign, multiblock) {
+    val name: String = sign.getLine(2).stripColor()
+    val maxShields: Double = (0.00671215 * starship.initialBlockCount.toDouble().pow(0.836512) - 0.188437)
+        get() = if (starship.initialBlockCount < 500) (1 - field + (1)) else field
+    val maxPower: Int = (starship.initialBlockCount.coerceAtLeast(500).d().pow(3.0 / 5.0) * 10000.0).roundToInt()
+        get() = if (starship.shields.size > maxShields) {
+            ((maxShields / starship.shields.size) * field).toInt()
+        } else if (starship.initialBlockCount <= 1000) {
+            (field / 2)
+        } else if (starship.type.eventship) {
+            (field * 2)
+        } else {
+            field
+        }
 
 
-	var power: Int = maxPower
-		set(value) {
-			field = value.coerceIn(0, maxPower)
-		}
-	var isReinforcementEnabled = multiblock.isReinforced
+    var power: Int = maxPower
+        set(value) {
+            field = value.coerceIn(0, maxPower)
+        }
+    var isReinforcementEnabled = multiblock.isReinforced
 
-	fun isReinforcementActive(): Boolean {
-		return isReinforcementEnabled && powerRatio > 0.8
-	}
+    fun isReinforcementActive(): Boolean {
+        return isReinforcementEnabled && powerRatio > 0.8
+    }
 
-	val powerRatio: Double get() = power.toDouble() / maxPower.toDouble()
+    val powerRatio: Double get() = power.toDouble() / maxPower.toDouble()
 
-    	fun getPowerUsage(power: Double): Int {
-		return if (starship.initialBlockCount < 1000)
-		 (power * 1500.0).toInt()
- 		else (power * 3000.0).toInt()
-	}
+    fun getPowerUsage(power: Double): Int {
+        return if (starship.initialBlockCount < 1000)
+            (power * 1500.0).toInt()
+        else (power * 3000.0).toInt()
+    }
 
-	abstract fun containsBlock(block: Block): Boolean
+    abstract fun containsBlock(block: Block): Boolean
 }
