f7e5ebb9bec96e4e02450aacf47c292bbe9142e2
NovaManGood2110 <128265875+NovaManGood2110@users.noreply.github.com>
2023-09-30 20:08:32 -0500

    Update IonCannonProjectile.kt

--- server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/IonCannonProjectile.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/starship/subsystem/weapon/projectile/IonCannonProjectile.kt
@@ -9,20 +9,20 @@ import org.bukkit.Location
 import org.bukkit.util.Vector
 
 // from left to right red - orange - yellow - green - blue - purple
-class ChaingunProjectile(
+class IonCannonProjectile(
 	starship: ActiveStarship,
 	loc: Location,
 	dir: Vector,
 	shooter: Controller?
 ) : LaserProjectile(starship, loc, dir, shooter) {
-	override val range: Double = IonServer.balancing.starshipWeapons.chaingun.range
-	override val speed: Double = IonServer.balancing.starshipWeapons.chaingun.speed
-	override val shieldDamageMultiplier: Int = IonServer.balancing.starshipWeapons.chaingun.shieldDamageMultiplier
+	override val range: Double = IonServer.balancing.starshipWeapons.IonCannon.range
+	override val speed: Double = IonServer.balancing.starshipWeapons.IonCannon.speed
+	override val shieldDamageMultiplier: Int = IonServer.balancing.starshipWeapons.IonCannon.shieldDamageMultiplier
 	override val color: Color
 		get() = if (starship!!.rainbowToggle) gayColors.random() else starship.weaponColor
-	override val thickness: Double = IonServer.balancing.starshipWeapons.chaingun.thickness
-	override val particleThickness: Double = IonServer.balancing.starshipWeapons.chaingun.particleThickness
-	override val explosionPower: Float = IonServer.balancing.starshipWeapons.chaingun.explosionPower
-	override val volume: Int = IonServer.balancing.starshipWeapons.chaingun.volume
-	override val soundName: String = IonServer.balancing.starshipWeapons.chaingun.soundName
+	override val thickness: Double = IonServer.balancing.starshipWeapons.IonCannon.thickness
+	override val particleThickness: Double = IonServer.balancing.starshipWeapons.IonCannon.particleThickness
+	override val explosionPower: Float = IonServer.balancing.starshipWeapons.IonCannon.explosionPower
+	override val volume: Int = IonServer.balancing.starshipWeapons.IonCannon.volume
+	override val soundName: String = IonServer.balancing.starshipWeapons.IonCannon.soundName
 }
